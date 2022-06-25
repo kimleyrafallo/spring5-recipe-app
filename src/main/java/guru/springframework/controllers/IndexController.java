e46f2df1065e3a655109ba2c6d66f5f0f416b838
kimleyrafallo <earlkimleyrafallo@gmail.com>
2022-06-25 20:39:57 +0800

    Added JPA query

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,35 @@
+package guru.springframework.controllers;
+
+import java.util.Optional;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.models.Category;
+import guru.springframework.models.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+
+public class IndexController {
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({ "", "/", "/index" })
+    public String getIndexPage(Model model) {
+
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println(category.get().getId());
+        System.out.println(uom.get().getId());
+        model.addAttribute("category", category.get().getId());
+        model.addAttribute("uom", uom.get().getId());
+
+        return "index";
+    }
+}
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,5 +1,7 @@
 package guru.springframework.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.models.UnitOfMeasure;
@@ -7,4 +9,5 @@ import guru.springframework.models.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
     
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
